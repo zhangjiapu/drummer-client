190f3522891fcf0550c466b2be52328e4bcaedee
zhangjiapu <1440409921@qq.com>
2019-02-21 00:01:22 +0800

    storeDetail finished

--- miniprogram/pages/detail/storedetail/storedetail.js
+++ miniprogram/pages/detail/storedetail/storedetail.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/detail/storedetail/storedetail.js
+// miniprogram/pages/detail/newsdetail/newsdetail.js
 Page({
 
   /**
@@ -19,44 +19,30 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    const db = wx.cloud.database({
+      env: 'drummer-2019'
+    });
+    let that = this;
+    // 获取课程信息
+    db.collection("store").doc(this.data.id).get({
+      success(res) {
+        that.setData({
+          store: res.data
+        })
+      }
+    });
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    // 获取购买说明
+    db.collection("buySpecification").get({
+      success(res) {
+        that.setData({
+          buySpecification: res.data[0]
+        })
+      }
+    });
 
   },
 
@@ -65,5 +51,43 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  go2comment: function () {
+    getApp().globalData.commentInfo.towho = "1";
+    getApp().globalData.commentInfo.whoid = this.data.store._id;
+    getApp().globalData.commentInfo.title = this.data.store.name;
+    wx.navigateTo({
+      url: "../../others/comment/comment"
+    })
+  },
+
+  go2contact: function () {
+    let that = this;
+    wx.makePhoneCall({
+      phoneNumber: getApp().globalData.customerService,
+    })
+  },
+
+  makeAppointment: function () {
+    let that = this;
+    wx.navigateTo({
+      url: "../../others/makeorder/makeorder?lessonname=" + that.data.lesson.name + "&lessonid=" + that.data.lesson._id + "&lessonprice=" + that.data.lesson.price,
+    })
+  },
+
+  showMap:function(){
+    let that = this;
+    wx.getLocation({//获取当前经纬度
+      type: 'wgs84', //返回可以用于wx.openLocation的经纬度，官方提示bug: iOS 6.3.30 type 参数不生效，只会返回 wgs84 类型的坐标信息  
+      success: function (res) {
+        wx.openLocation({//​使用微信内置地图查看位置。
+          latitude: Number(that.data.store.location.latitude),//要去的纬度-地址
+          longitude: Number(that.data.store.location.longitude),//要去的经度-地址
+          name: that.data.store.name,
+          address: that.data.store.name
+        })
+      }
+    })
   }
 })
